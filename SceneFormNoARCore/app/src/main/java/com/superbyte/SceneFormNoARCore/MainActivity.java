0e480c45c7de3cae291fcafcfca447b83b80d4d0
徐旭 <xubeibei1@xiaomi.com>
2020-06-22 14:21:12 +0800

    添加touch事件

--- SceneFormNoARCore/app/src/main/java/com/superbyte/SceneFormNoARCore/MainActivity.java
+++ SceneFormNoARCore/app/src/main/java/com/superbyte/SceneFormNoARCore/MainActivity.java
@@ -2,15 +2,21 @@ package com.superbyte.SceneFormNoARCore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.SceneView;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 
@@ -25,8 +31,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sceneView = (SceneView) findViewById(R.id.sceneView);
+        Camera camera = sceneView.getScene().getCamera();
+        camera.setLocalRotation(Quaternion.axisAngle(Vector3.right(), -30.0f));
+
         scene = sceneView.getScene();
+        scene.setOnTouchListener(new Scene.OnTouchListener() {
+            @Override
+            public boolean onSceneTouch(HitTestResult hitTestResult, MotionEvent motionEvent) {
+                onTouch(hitTestResult, motionEvent);
+                return false;
+            }
+        });
+
+        //renderObject(Uri.parse("cupcake.sfb")); // Render the object
+    }
+
+    public boolean onTouch(HitTestResult hitTestResult, MotionEvent motionEvent) {
         renderObject(Uri.parse("cupcake.sfb")); // Render the object
+        return false;
     }
 
     @Override
